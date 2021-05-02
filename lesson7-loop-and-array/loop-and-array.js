dbf7f78b10e5030df35df50cd3c01ad622e5a801
RReiso <ruta.littlemore@gmail.com>
2021-05-02 00:17:13 -0400

    print every third number in two ways - automatically and by passing an array as an argument

---
+++ lesson7-loop-and-array/loop-and-array.js
@@ -0,0 +1,38 @@
+//print every third number from 1 to 20:
+function printNumber() {
+	for (let i = 1; i < 21; i = i + 3) {
+		console.log(i);
+	}
+}
+printNumber(); //calling function 
+
+//print every third number from an array:
+const printEveryThird = (array) => {
+	for (let i = 0; i < array.length; i = i + 3) {
+		console.log(array[i]);
+	}
+};
+
+//calling function and passing an array as an argument:
+printEveryThird([
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+	10,
+	11,
+	12,
+	13,
+	14,
+	15,
+	16,
+	17,
+	18,
+	19,
+	20,
+]);
