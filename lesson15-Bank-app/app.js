2e842e2e1fedf5a4d51ad663cc19bb8959748b84
RReiso <rutareiso@protonmail.com>
2021-06-04 23:22:01 -0400

    Refactor app.js

--- lesson15-Bank-app/app.js
+++ lesson15-Bank-app/app.js
@@ -1,3 +1,11 @@
+//***GLOBAL VARIABLES***//
+
+const serverBaseUrl = "http://localhost:5000/api";
+const storageKey = "savedAccount";
+
+
+//***ROUTING***//
+
 const routes = {
 	"/login": {
 		templateId: "login",
@@ -10,14 +18,77 @@ const routes = {
 	},
 };
 
-const storageKey = "savedAccount";
+function navigate(path) {
+	//Add path to the browser history:
+	window.history.pushState({}, path, window.location.origin + path);
+	updateRoute();
+}
 
+function updateRoute() {
+	//Find current route:
+	const path = window.location.pathname;
+	console.log(path);
+	const route = routes[path];
+	console.log(route);
+
+	//If route doesn't exist, return to "/login":
+	if (!route) {
+		return navigate("/login");
+	}
+
+	//Find the template according to the current route:
+	const template = document.getElementById(route.templateId);
+	const view = template.content.cloneNode(true);
+	const app = document.getElementById("app");
+	const title = document.querySelector("title");
+
+	//Display the appropriate template:
+	app.innerHTML = "";
+	app.appendChild(view);
+
+	title.innerHTML = route.title;
+	console.log(title.innerHTML + " is shown.");
+
+	//if the route is "/dashboard", execute refresh() function:
+	if (typeof route.init === "function") {
+		route.init();
+	}
+}
+
+//***API REQUESTS***//
+
+//Sending a GET request to check if user exists and read the data if they do:
+async function getAccount(user) {
+	try {
+		const response = await fetch(
+			serverBaseUrl + "/accounts/" + encodeURIComponent(user)
+		);
+		return await response.json();
+	} catch (error) {
+		return { error: error.message || "Unknown error" };
+	}
+}
+
+//Sending a POST request to create a new user's account
+async function createAccount(account) {
+	try {
+		const response = await fetch(serverBaseUrl + "/accounts", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: account,
+		});
+		return await response.json();
+	} catch (error) {
+		return { error: error.message || "Unknown error" };
+	}
+}
+
+//***STATE***//
 
 //state that represents user account's current data:
 let state = Object.freeze({
 	account: null,
 });
-
 
 //Update user's account data:
 function updateState(property, newData) {
@@ -26,68 +97,16 @@ function updateState(property, newData) {
 		[property]: newData,
 	});
 
-  //Update loclStorage with user's account data
+	//Update loclStorage with user's account data
 	localStorage.setItem(storageKey, JSON.stringify(state.account));
 }
 
-//Add text or node to the element:
-function updateElement(id, textOrNode) {
-	const element = document.getElementById(id);
-	element.textContent = "";
-	element.append(textOrNode);
-}
-
-function updateRoute() {
-  //Find current route:
-	const path = window.location.pathname;
-	console.log(path);
-	const route = routes[path];
-	console.log(route);
-
-  //If route doesn't exist, return to "/login":
-	if (!route) {
-		return navigate("/login");
-	}
-
-  //Find the template according to the current route:
-	const template = document.getElementById(route.templateId);
-	const view = template.content.cloneNode(true);
-	const app = document.getElementById("app");
-	const title = document.querySelector("title");
-
-  //Display the appropriate template:
-	app.innerHTML = "";
-	app.appendChild(view);
-
-	title.innerHTML =
-		route.title;
-	console.log(title.innerHTML + " is shown.");
-
-  //if the route is "/dashboard", execute refresh() function:
-	if (typeof route.init === "function") {
-		route.init();
-	}
-}
-
-
-function navigate(path) {
-  //Add path to the browser history:
-	window.history.pushState({}, path, window.location.origin + path);
-	updateRoute();
-}
-
-async function refresh() {
-  //Get user's account data and update stae:
-	await updateAccountData();
-
-	//Display user's account data according to the current state:
-	updateDashboard();
-}
+//***LOGIN LOGOUT REGISTER***//
 
 async function login() {
 	const loginForm = document.getElementById("loginForm");
 	const user = loginForm.user.value;
-  //Chech if user exists and read data:
+	//Chech if user exists and read data:
 	const data = await getAccount(user);
 
 	if (data.error) {
@@ -102,16 +121,51 @@ async function login() {
 	updateDashboard();
 }
 
-//Sending a GET request to check if user exists and read the data if they do:
-async function getAccount(user) {
-	try {
-		const response = await fetch(
-			"//localhost:5000/api/accounts/" + encodeURIComponent(user)
-		);
-		return await response.json();
-	} catch (error) {
-		return { error: error.message || "Unknown error" };
+async function register() {
+	//Get data from the registration form:
+	const registerForm = document.getElementById("registerForm");
+	const formData = new FormData(registerForm);
+	const data = Object.fromEntries(formData);
+	const jsonData = JSON.stringify(data);
+
+	//Use new user's data to create an account:
+	const result = await createAccount(jsonData);
+
+	if (result.error) {
+		return console.log("An error occured:", result.error);
 	}
+
+	//Update user's account information:
+	updateState("account", result);
+	console.log("Account created!", result);
+	//Redirect:
+	navigate("/dashboard");
+	//Display user's account data according to the current state:
+	updateDashboard();
+}
+
+function logout() {
+	//Set user's account data to null (clear the data):
+	updateState("account", null);
+	//Redirect:
+	navigate("/login");
+}
+
+//***UPDATE FUNCTIONS***/
+
+//Add text or node to the element:
+function updateElement(id, textOrNode) {
+	const element = document.getElementById(id);
+	element.textContent = "";
+	element.append(textOrNode);
+}
+
+async function refresh() {
+	//Get user's account data and update stae:
+	await updateAccountData();
+
+	//Display user's account data according to the current state:
+	updateDashboard();
 }
 
 async function updateAccountData() {
@@ -127,45 +181,6 @@ async function updateAccountData() {
 	//Update user's account information:
 	updateState("account", data);
 }
-
-
-async function register() {
-  //Get data from the registration form:
-	const registerForm = document.getElementById("registerForm");
-	const formData = new FormData(registerForm);
-	const data = Object.fromEntries(formData);
-	const jsonData = JSON.stringify(data);
-
-  //Use new user's data to create an account:
-	const result = await createAccount(jsonData);
-
-	if (result.error) {
-		return console.log("An error occured:", result.error);
-	}
-
-  //Update user's account information:
-	updateState("account", result);
-	console.log("Account created!", result);
-  //Redirect:
-	navigate("/dashboard");
-  //Display user's account data according to the current state:
-	updateDashboard();
-}
-
-//Sending a POST request to create a new user's account
-async function createAccount(account) {
-	try {
-		const response = await fetch("//localhost:5000/api/accounts", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: account,
-		});
-		return await response.json();
-	} catch (error) {
-		return { error: error.message || "Unknown error" };
-	}
-}
-
 
 //Display account's data based on the current state of the account:
 function updateDashboard() {
@@ -199,17 +214,12 @@ function createTransactionRow(transaction) {
 	return transactionRow;
 }
 
-function logout() {
-	//Set user's account data to null (clear the data):
-	updateState("account", null);
-  //Redirect:
-	navigate("/login");
-}
+//***INITIALIZING CODE***//
 
 function init() {
 	const savedAccount = localStorage.getItem(storageKey);
 
-  //If there is data in localStorage, update user's account information in state:
+	//If there is data in localStorage, update user's account information in state:
 	if (savedAccount) {
 		updateState("account", JSON.parse(savedAccount));
 	}
@@ -218,5 +228,4 @@ function init() {
 	updateRoute();
 }
 
-//initialize code:
 init();
