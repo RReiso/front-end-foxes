45de6c6d070fef1c9751bb89c1283089651e38a8
RReiso <rutareiso@protonmail.com>
2021-06-14 22:21:49 -0400

    components and state

--- reactapp/portfolio/src/App.js
+++ reactapp/portfolio/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import {
+	heroData,
+	aboutData,
+	projectsData,
+	contactData,
+	footerData,
+} from "./mock/data";
+import Hero from "./components/Hero";
+// import About from "./components/About";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [hero, setHero] = useState({});
+	// const [about, setAbout] = useState({});
+	// const [projects, setProject] = useState([]);
+	// const [contact, setContact] = useState({});
+	// const [footer, setFooter] = useState({});
+
+	useEffect(() => {
+		setHero({ ...heroData });
+		// setAbout({ ...aboutData });
+		// setProject({ ...projectsData });
+		// setContact({ ...contactData });
+		// setFooter({ ...footerData });
+	}, []);
+
+	return (
+		<div>
+			<Hero hero={hero} />
+{/* 
+			<About about={about} />
+			<Projects projects={projects} />
+			<Contact contact={contact} />
+			<Footer footer={footer} />
+       */}
+		</div>
+	);
 }
 
 export default App;
