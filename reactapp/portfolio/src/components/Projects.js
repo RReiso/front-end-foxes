77c63527487634d51a1aafadfbde7aa91f585c29
RReiso <rutareiso@protonmail.com>
2021-06-17 22:14:49 -0400

    Add Projects component

---
+++ reactapp/portfolio/src/components/Projects.js
@@ -0,0 +1,16 @@
+const Projects = ({ projects }) => {
+	return (
+		<div>
+			{projects.map((project) => (
+				<section>
+					<h3>{project.title}</h3>
+					<p>{project.info}</p>
+					<a href={project.url}>View {project.title} live!</a>
+					<a href={project.repo}>{project.title} on Github!</a>
+				</section>
+			))}
+		</div>
+	);
+};
+
+export default Projects;
