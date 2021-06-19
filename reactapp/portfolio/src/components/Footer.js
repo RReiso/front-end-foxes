48945319ce8d5fb14d55cd9b16367786e6f99180
RReiso <rutareiso@protonmail.com>
2021-06-19 00:17:59 -0400

    Refactor code

--- reactapp/portfolio/src/components/Footer.js
+++ reactapp/portfolio/src/components/Footer.js
@@ -1,20 +1,7 @@
 const Footer = ({ footer }) => {
-	console.log(footer);
-	console.log(typeof footer);
-	let array = [];
-	array = Array.from(footer);
-	// footer.map((f) => {
-	//   array.push({name:f.name, url:f.url});
-	//   console.log(f.name)
-	// })
-	console.log(array);
+	array = Array.from(footer);//Create array
 	return (
 		<div className="contact_links">
-			{/* {array.map((f) => (
-				<div>
-					<h3>{f.name}</h3>
-				</div>
-			))} */}
 			{array.map((link) => (
 				<a href={link.url} className="link">
 					<p className="sr-only">Ruta's {link.name} profile</p>
